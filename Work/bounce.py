dab9730060c0de7fb1072c34e40f88e50ec20909
Sundaresan Vikirapandiyam <sundaresan.vikirapandiyam1@t-mobile.com>
2021-11-18 14:06:43 -0800

    chapter 1 completed

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 # meters
+bounce_height = 3/5 # bounce height
+bounce = 1
+
+while bounce <= 10:
+	height = height * bounce_height
+	bounce = bounce + 1
+	print (bounce, round(height,4))
+
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,35 @@
-# pcost.py
-#
-# Exercise 1.27
+#pcost.py
+#calculate total cost of portfolio in portfolio.csv
+#11/17/2021
+
+import sys
+#import os
+#import gzip
+import csv
+
+def portfolio_cost(filename):
+	f = open(filename, 'rt')
+	rows = csv.reader(f)
+	headers = next(rows)
+	Total = 0
+
+	for row in rows:
+	#	row = line.split(',')
+		print(row)
+		try:
+			shares = int(row[1])
+			cost = float(row[2])
+			Total = Total + (shares * cost)
+		except ValueError:
+			print("Couldn't parse", row)
+
+	f.close()
+	return Total
+
+if len(sys.argv) == 2:
+	filename = sys.argv[1]
+else:
+	filename = 'Data/portfolio.csv'
+
+cost1 = portfolio_cost(filename)
+print('Total cost:', cost1)
